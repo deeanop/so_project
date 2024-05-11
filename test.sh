72c1d5d7d000833f6b1ef1562be3ab3210bf6097
deean <deeanoprea55544@gmail.com>
2024-05-11 19:18:12 +0300

    versiunea finala a script-ului

--- test.sh
+++ test.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-a="$1" #calea catre fisierul malitios
-b="$2" #numele directorului in care vom muta
-chmod 777 "$a"
-cd $(dirname "a")
-if test grep -q "corrupted" "$a" || grep -q "dangerous" "$a" || grep -q "risk" "$a" || grep -q "attack" "$a" || grep -q "malware" "$a" || grep -q "malicious" "$a" || grep -P -q '[^\x00-\x7F]' "$a"; then
-    echo "Fisierul cu calea $a este periculos"
-    mv "$(basename "$a")" "$b/"
-    echo "Fisierul cu calea $a a fost mutat in $b"
+# Verificați dacă un fișier este suspect sau periculos
+
+a="$1"  # Calea către fișier
+ok=1
+chmod 777 $a
+
+# Numărul de linii, cuvinte și caractere din fișier
+line_count=$(wc -l < "$a")
+word_count=$(wc -w < "$a")
+character_count=$(wc -c < "$a")
+
+# Verificați dacă fișierul este suspect
+if [ $line_count -lt 3 ] && [ $word_count -gt 1000 ] && [ $character_count -gt 2000 ]; then
+    echo "Fisier suspect"
+    # Verifică dacă fișierul conține cuvinte cheie pentru pericol
+    if grep -q "corrupted" "$a" || grep -q "dangerous" "$a" || grep -q "risk" "$a" || grep -q "attack" "$a" || grep -q "malware" "$a" || grep -q "malicious" "$a" || grep -P -q '[^\x00-\x7F]' "$a"; then
+        echo "Fisier periculos"
+        ok=2
+    fi
+else
+    echo "Fisier nepericulos"
 fi
-cd ..
+
+exit $ok
